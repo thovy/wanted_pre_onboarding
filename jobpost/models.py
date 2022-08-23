9fac66f49ae0fd9596a07170659ebb877e0ad796
Thovy <kingvely151113@gmail.com>
2022-08-24 08:32:45 +0900

    Feat: 채용공고 작성 시간 자동 기록 추가

--- jobpost/models.py
+++ jobpost/models.py
@@ -17,5 +17,9 @@ class JobPost(models.Model):
     skill = models.CharField(max_length=100)
     article = models.TextField()
 
+    # 채용공고 작성, 수정 시간
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.name
